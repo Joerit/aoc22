018e9476049ed1ffed87dc9277c595fce810fdb2
ratty <ratty@inuits.eu>
2022-12-01 14:53:08 +0100

    day1pt2

--- elixir/day1/lib/day1.ex
+++ elixir/day1/lib/day1.ex
@@ -8,10 +8,20 @@ defmodule Day1 do
   require Enum
 
   def start(_type, _args) do
-    read_input()
+    input = read_input()
+    
+    IO.puts "pt1: "
+    input 
     |> day1 
     |> inspect
     |> IO.puts
+  
+    IO.puts "pt2: "
+    input 
+    |> day1p2 
+    |> inspect
+    |> IO.puts
+
   end
 
   def day1(input) do
@@ -19,9 +29,20 @@ defmodule Day1 do
     |> String.split("\n\n", trim: true)
     |> Enum.map(&to_elf/1)
     |> Enum.map(&to_total/1)
-    |> IO.inspect
     |> max
-    |> IO.inspect
+  end
+
+  def day1p2(input) do
+    input
+    |> String.split("\n\n", trim: true)
+    |> Enum.map(&to_elf/1)
+    |> Enum.map(&to_total/1)
+    |> Enum.sort(:desc)
+    |> sum_first_3
+  end
+
+  def sum_first_3( [a , b ,  c | _] ) do
+    a + b + c 
   end
 
   def max( [head] ) do head end
@@ -37,7 +58,6 @@ defmodule Day1 do
   def to_elf(strelf) do
     strelf
     |> String.split("\n", trim: true)
-    |> IO.inspect
     |> Enum.map(&String.to_integer/1)
   end
 
@@ -45,14 +65,4 @@ defmodule Day1 do
     elf 
     |> Enum.sum
   end
-
-
-
-
-
-
-
-
-
-
 end
