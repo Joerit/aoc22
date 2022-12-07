0e966dc031a189f5219391c6d115e93380973116
ratty <ratty@inuits.eu>
2022-12-07 13:31:14 +0100

    moving some to Helpers

--- helper/lib/helper.ex
+++ helper/lib/helper.ex
@@ -2,6 +2,26 @@ defmodule Helper do
   @moduledoc """
   Some helper functions for aoc
   """
+  def run(pt1, pt2) do
+    "PT1: " |> IO.puts 
+    "# test:" |> IO.puts
+    read_input("./testinput")
+    |> pt1.()
+
+    "# input:" |> IO.puts
+    read_input("./input")
+    |> pt1.()
+    
+    "PT2: " |> IO.puts 
+    "# test:" |> IO.puts
+    read_input("./testinput")
+    |> pt2.()
+
+    "# input:" |> IO.puts
+    read_input("./input")
+    |> pt2.()
+  end
+
   def read_lines() do
     read_input("./input")
     |> String.split("\n", trim: true)
@@ -15,7 +35,23 @@ defmodule Helper do
   def read_input(file \\ "./input") do
     File.read!(file)
   end
+  
+  def list_to_map(list, start \\ 0) do
+    {_, ret} = list
+               |> Enum.reverse
+               |> _list_to_map(start)
+    ret
+  end
+  def _list_to_map(list, start \\ 0)
+  def _list_to_map([head], start) do
+    { start+1, %{start => head }}
+  end
+  def _list_to_map([ head | tail ], start) do
+    { index, map } = _list_to_map(tail, start)
+    {( index+1 ), (Map.update( map, index, head, fn x -> x end ))}
+  end
 
-
-
+  def pop([head | tail]) do
+    {[ head ], tail}
+  end
 end
--- helper/test/helper_test.exs
+++ helper/test/helper_test.exs
@@ -7,7 +7,14 @@ defmodule HelperTest do
   end
 
   test "read_lines" do
-    assert Helper.read_lines() == ["a", "b", "c", ""]
+    assert Helper.read_lines() == ["a", "b", "c"]
+  end
+
+  test "list_to_map" do
+    assert Helper.list_to_map([1, 2, 3, 4]) ==
+      %{0 => 1, 1 => 2, 2 => 3, 3 => 4}
+    assert Helper.list_to_map([1, 2, 3, 4], 2) ==
+      %{2 => 1, 3 => 2, 4 => 3, 5 => 4}
   end
 
 end
